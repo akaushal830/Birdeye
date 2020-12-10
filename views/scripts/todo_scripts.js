6bc618805465dfb2ec5979a0134175b46c4b678f
Abhishek Kaushal <akaushal830@gmail.com>
2020-12-10 14:22:15 +0530

    OO Todo

--- views/scripts/todo_scripts.js
+++ views/scripts/todo_scripts.js
@@ -1,15 +1,42 @@
+// function showDetails(id)
+// {
+//     console.log('from onclick', id);
+//     window.location.href = "http://localhost:3000/todo/"+id;
+// }
 
-function showDetails(id)
-{
-    console.log('from onclick', id);
-    window.location.href = "http://localhost:3000/todo/"+id;
+// function editDetails(id)
+// {
+//     console.log('from onclick', id);
+//     window.location.href = "http://localhost:3000/todo/"+id+"/edit";
+// }
+
+
+class Todo {
+    showDetails(id) {
+        console.log('from onclick', id);
+        window.location.href = "http://localhost:3000/todo/" + id;
+    }
+
+    editDetails(id) {
+        console.log('from onclick', id);
+        window.location.href = "http://localhost:3000/todo/" + id + "/edit";
+    }
 }
 
-function editDetails(id)
-{
-    console.log('from onclick', id);
-    window.location.href = "http://localhost:3000/todo/"+id+"/edit";
+let obj = new Todo();
+
+if(document.getElementById("hs") !== null){
+    document.getElementById("hs").addEventListener('click', (e) => {
+        obj.showDetails(e.target.id);
+    });
 }
+
+if(document.getElementById("edb") !== null){
+    document.getElementById("edb").addEventListener('click', (e) => {
+        obj.editDetails(document.getElementById("edid").innerText);
+    });
+}
+
 
 
 
